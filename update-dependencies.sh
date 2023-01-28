6eeb997a86d9a3a36b45a641be84b6f0172ea9a1
Rafael Fiume <rafael.fiume@yahoo.it>
2023-01-28 11:03:46 +0000

    Config scala-steward to update project dependencies

---
+++ update-dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+LOGIN="rafaelfiume"
+EMAIL="rafael.fiume@yahoo.it"
+STEWARD_DIR="$HOME/rf-workspace/scala-steward/"
+
+sbt stage
+
+./modules/core/.jvm/target/universal/stage/bin/scala-steward \
+  --workspace  "$STEWARD_DIR/workspace" \
+  --repos-file "$STEWARD_DIR/repos.md" \
+  --repo-config "$STEWARD_DIR/.scala-steward.conf" \
+  --git-author-email ${EMAIL} \
+  --vcs-api-host "https://api.github.com" \
+  --vcs-login ${LOGIN} \
+  --git-ask-pass "$HOME/.github/tokens/rafaelfiume.sh" \
+  --do-not-fork
